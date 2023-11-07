c437b0bff471c63da8592609aa97957f4b067c21
Anant-mishra1729 <amishra1577@gmail.com>
2023-11-07 21:47:36 +0530

    Resolved bug

--- form_autofill.py
+++ form_autofill.py
@@ -2,7 +2,7 @@
 from selenium import webdriver
 import time
 from selenium.webdriver.common.by import By
-import random
+import numpy as np
 
 # Load firefox webdriver
 driver = webdriver.Firefox()
@@ -17,6 +17,24 @@ INPUT_CLASS = "KHxj8b"
 RADIO_BUTTON_CLASS = "docssharedWizToggleLabeledContainer"
 SUBMIT_BUTTON_CLASS = "NPEfkd"
 
+# Responses to exclude to manipulate the data
+EXCLUDE_RESPONSES = [
+    "Other:",
+    "Above 50",
+    "35-50",
+    "Under 18",
+    "Doctorate",
+    "Not at all familiar",
+    "Encryption",
+    "I'm not familiar with any of these terms",
+    "Yes",
+    "High School or less",
+    "Some College or Associate's Degree",
+    "Master's Degree",
+    "High School or less",
+]
+
+
 def fillForm():
     question_containers = driver.find_elements(By.CLASS_NAME, CONTAINER_CLASS)
 
@@ -25,28 +43,37 @@ def fillForm():
     radio_buttons_stored = []
 
     for question_container in question_containers:
-        question_name = question_container.find_element(By.CLASS_NAME, "M7eMe").text
+        # To get question name
+        # question_name = question_container.find_element(By.CLASS_NAME, "M7eMe").text
 
         # Store all input fields in a list
         # For input fields
         input_fields = question_container.find_elements(By.CLASS_NAME, INPUT_CLASS)
-        if(len(input_fields) > 0):
+        if len(input_fields) > 0:
             input_fields_stored.extend(input_fields)
 
         # For radio buttons
-        radio_buttons = question_container.find_elements(By.CLASS_NAME, RADIO_BUTTON_CLASS)
+        radio_buttons = question_container.find_elements(
+            By.CLASS_NAME, RADIO_BUTTON_CLASS
+        )
 
-        # Remove radio buttons with "Other" option
+        required_buttons = []
         for radio_button in radio_buttons:
-            if(radio_button.text == "Other:"):
-                radio_buttons.remove(radio_button)
+            if radio_button.text not in EXCLUDE_RESPONSES:
+                required_buttons.append(radio_button)
 
-        if (len(radio_buttons) > 0):
-            radio_buttons_stored.append(radio_buttons)
+        if len(radio_buttons) > 0:
+            radio_buttons_stored.append(required_buttons)
+
+    for rbs in radio_buttons_stored:
+        print("[", end="")
+        for rb in rbs:
+            print(rb.text)
+        print("]")
 
     for radio_buttons in radio_buttons_stored:
         # Select random radio button
-        random_radio_button = random.choice(radio_buttons)
+        random_radio_button = np.random.choice(radio_buttons)
         random_radio_button.click()
 
     for input_field in input_fields_stored:
@@ -59,7 +86,8 @@ def fillForm():
     another_response = driver.find_element(By.LINK_TEXT, "Submit another response")
     another_response.click()
 
-for i in range(2):
+
+for i in range(50):
     time.sleep(0.5)
     fillForm()
 
